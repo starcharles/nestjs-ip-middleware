5d4e2cad1d48cdf0d6155b0c8a0b1eb8f0cc1abd
naoto <stardustforever012@gmail.com>
2021-12-10 06:48:16 +0900

    feat: changes   - add README   - add types   - add tests   - add needed packages

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,5 +1,41 @@
+import { Test } from '@nestjs/testing';
+import * as request from 'supertest';
+import { TestModule } from './util/test.module';
+
 describe('test', () => {
-  it('should pass', () => {
-    expect(1 + 1).toBe(2);
+  describe('1+1', () => {
+    it('should pass', () => {
+      expect(1 + 1).toBe(2);
+    });
+  });
+  describe('Middleware', () => {
+    let server;
+    beforeAll(async () => {
+      const module = await Test.createTestingModule({
+        imports: [TestModule],
+      }).compile();
+      const app = module.createNestApplication();
+      await app.init();
+      server = app.getHttpServer();
+    });
+
+    afterAll(() => {
+      server.close();
+    });
+
+    it('GET /hello', () => {
+      return request(server)
+        .get('/hello')
+        .expect(200)
+        .expect('hello');
+    });
+
+    it('GET /ip', () => {
+      return request(server)
+        .get('/ip')
+        .set('X-Forwarded-For', '10.10.10.10')
+        .expect(200)
+        .expect('10.10.10.10');
+    });
   });
 });
